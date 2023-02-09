943f325b8648404f7cf1d51738a8b9c755be70bd
IbrahimBasit5802 <ibasit050802@gmail.com>
2023-02-09 12:53:08 +0500

    changed app title

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Property Guru',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Pallete.backgroundColor,
       ),
