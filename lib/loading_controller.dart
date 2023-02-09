cde42b7025bb96851ddbf80a0de15050e303f546
IbrahimBasit5802 <ibasit050802@gmail.com>
2023-02-09 12:25:42 +0500

    loading controller

---
+++ lib/loading_controller.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+
+
+class LoadingController extends GetxController {
+  bool _isLoading = false;
+  bool get invalid => _isLoading;
+  void isLoading() {
+    _isLoading = true;
+    update();
+  }
+
+  void isNotLoading() {
+    _isLoading = false;
+    update();
+  }
+}
