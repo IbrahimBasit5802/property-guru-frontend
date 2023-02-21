3bdef75eb67860b849572406e0d98eb514345dae
IbrahimBasit5802 <ibasit050802@gmail.com>
2023-02-21 19:39:17 +0500

    announcement screen

--- lib/DummyScreen.dart
+++ lib/DummyScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:naseeb/pallete.dart';
+import 'package:naseeb/widgets/gradient_button.dart';
 class DummyScreen extends StatelessWidget {
   const DummyScreen({Key? key}) : super(key: key);
 
@@ -6,9 +8,31 @@ class DummyScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Pallete.backgroundColor,
+      ),
       home: Scaffold(
         body: Center(
-          child: Text('Dummy Screen'),
+          child: Column(
+            children: [
+              Image.asset('assets/images/signin_balls.png'),
+              const Text(
+                'Notice Board.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                ),
+              ),
+              const SizedBox(height: 100,),
+              GradientButton(ButtonText: "Announcements"),
+              const SizedBox(height: 20,),
+              GradientButton(ButtonText: "Dummy Button"),
+              const SizedBox(height: 20,),
+              GradientButton(ButtonText: "Dummy Button"),
+              const SizedBox(height: 20,),
+              GradientButton(ButtonText: "Dummy Button"),
+            ],
+          ),
         ),
       ),
     );
